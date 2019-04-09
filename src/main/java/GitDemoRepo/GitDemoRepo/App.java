40573e05eeb09779d15a4223655a62338a9abd17
Praveen Koppula <koppula.praveenreddy@gmail.com>
2019-04-08 21:58:15 -0700

    git demo repo by praveen

---
+++ src/main/java/GitDemoRepo/GitDemoRepo/App.java
@@ -0,0 +1,13 @@
+package GitDemoRepo.GitDemoRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
